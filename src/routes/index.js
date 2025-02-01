9fe3ee73152aa2c4aeaa26692221858bd21845eb
aldairmb <aldairmorales.banda@gmail.com>
2025-02-01 13:23:38 -0700

    .

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,4 +7,10 @@ router.get('/', (req, res) => {
     res.render('index', { title: 'Home Page' });
 });
 
+// The about route
+router.get('/about', (req, res) => {
+    res.render('about', { title: 'About' });
+});
+
+
 export default router;
